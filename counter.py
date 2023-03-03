e8e728c1e87dfe254666f9a89e7960e0e27b6469
Alex Lo Storto <lostorto.business@gmail.com>
2023-03-03 22:42:30 +0000

    Add counter script

---
+++ counter.py
@@ -0,0 +1,36 @@
+import os
+import re
+
+
+def countFiles(path):
+    totalMessages = 0
+
+    messages = {}
+
+    regex = "\d{2}\/\d{2}\/\d{4},{1} {1}\d{2}:{1}\d{2} {1}-{1} {1}"
+
+    with open(path, encoding="utf8") as textfile:
+        for line in textfile:
+            position = re.search(regex, line)
+            if position is not None and line.count(':') >= 2:
+                totalMessages += 1
+                username = ":".join(line.split(':', 2)[:2])[position.end():]
+                if username in messages:
+                    messages[username] += 1
+                else:
+                    messages[username] = 1
+
+    for username, number in messages.items():
+        print(f"{username}: {number}")
+
+    print(f"Total messages: {totalMessages}")
+
+
+def openRoot(root):
+    for path, _, files in os.walk(root):
+        for name in files:
+            if name.endswith(".txt"):
+                countFiles(os.path.join(path, name))
+
+
+openRoot(os.path.dirname(os.path.abspath(__file__)))
