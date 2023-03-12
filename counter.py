aaf6f1f1c8356df226927f0488cecc57db097e56
Alex Lo Storto <lostorto.business@gmail.com>
2023-03-12 21:02:20 +0000

    Add dynamic function options and text counter

--- counter.py
+++ counter.py
@@ -2,34 +2,54 @@ import os
 import re
 
 
-def countMessages(path, logDate=False):
+def countMessages(path, logDate=False, textCounter=False):
     totalMessages = 0
+    totalTextCount = 0
 
     messages = {}
+    textCount = {}
 
     regex = "\d{2}\/\d{2}\/\d{4}, \d{2}:\d{2} - "
+
+    if textCounter:
+        textToFind = input("Text to count: ")
+
+    print("\n\n---STATISTICS---")
 
     with open(path, encoding="utf8") as textfile:
         date = ''
         dailyMessages = {}
+        dailyTextCounter = {}
         for line in textfile:
             position = re.search(regex, line)
             if position is not None and line.count(':') >= 2:
                 totalMessages += 1
                 username = ":".join(line.split(':', 2)[:2])[position.end():]
+                message = ":".join(line.split(':', 2)[2:])[1:]
                 if username in messages:
                     messages[username] += 1
                 else:
                     messages[username] = 1
 
                 # Daily message counter
-                if not logDate:
-                    continue
+                if logDate:
+                    if username in dailyMessages:
+                        dailyMessages[username] += 1
+                    else:
+                        dailyMessages[username] = 1
 
-                if username in dailyMessages:
-                    dailyMessages[username] += 1
-                else:
-                    dailyMessages[username] = 1
+                # Text counter
+                if textCounter and textToFind.lower() in message.lower():
+                    totalTextCount += 1
+                    if username in textCount:
+                        textCount[username] += 1
+                    else:
+                        textCount[username] = 1
+                    if username in dailyTextCounter:
+                        dailyTextCounter[username] += 1
+                    else:
+                        dailyTextCounter[username] = 1
+
                 if ":".join(line.split(':', 2)[:2])[:10] != date:
                     if date == '':
                         date = ":".join(line.split(':', 2)[:2])[:10]
@@ -38,8 +58,12 @@ def countMessages(path, logDate=False):
                     date = ":".join(line.split(':', 2)[:2])[:10]
                     for username, number in dailyMessages.items():
                         print(f"{username}: {number:,}")
+                    for username, number in dailyTextCounter.items():
+                        print(f"Times {username} said '{textToFind}': {number:,}")
                     dailyMessages = {}
+                    dailyTextCounter = {}
                     print()
+
         if logDate:
             print(date)
             date = ":".join(line.split(':', 2)[:2])[:10]
@@ -52,8 +76,12 @@ def countMessages(path, logDate=False):
         print("\n---TOTAL---")
     for username, number in messages.items():
         print(f"{username}: {number:,}")
+    for username, number in textCount.items():
+        print(f"Times {username} said '{textToFind}': {number:,}")
 
     print(f"Total messages: {totalMessages:,}")
+    if textCounter:
+        print(f"Total times '{textToFind}' was said: {totalTextCount:,}")
 
 
 def openRoot(root):
@@ -72,6 +100,20 @@ def openRoot(root):
                 userInput = int(userInput)
                 return userInput
 
+    def yesNo(question):
+        userInput = input(question)
+        print()
+
+        while True:
+            # Check input is acceptable
+            if userInput == '':
+                print(f"Don't leave me empty!")
+                userInput = input(question)
+            elif userInput.lower() in ['y', 'ye', 'yes', 'yeah']:
+                return True
+            elif userInput.lower() in ['n', 'no', 'nop', 'nope']:
+                return False
+
     txtFiles = []
     for path, _, files in os.walk(root):
         for file in files:
@@ -84,23 +126,19 @@ def openRoot(root):
     file = getInput(txtFiles, "Choose which file you want to use: ")
     print(f"You chose '{txtFiles[file-1][1]}'\n")
 
-    functions = [
-        "countMessages",
-        "displayDays"
+    options = [
+        "displayDays",
+        "textCounter"
     ]
 
-    # Choose function
-    for i in range(2):
-        print(f"{i+1}) {functions[i]}")
-    function = getInput(functions, "Choose which function you want to use: ")
-    print(f"You chose '{functions[function-1]}'\n")
+    parameters = []
 
-    print("\n\n---STATISTICS---")
-    if function == 1:
-        countMessages(os.path.join(txtFiles[file-1][0], txtFiles[file-1][1]))
-    elif function == 2:
-        countMessages(os.path.join(txtFiles[file-1][0], txtFiles[file-1][1]), True)
-    print("\n\n")
+    # Choose function
+    for i in range(len(options)):
+        parameters.append(yesNo(f"Do you want to use {options[i]}: "))
+
+    countMessages(os.path.join(txtFiles[file-1][0], txtFiles[file-1][1]), *parameters)
+    print("\n")
 
     input("Press ENTER to exit")
 
